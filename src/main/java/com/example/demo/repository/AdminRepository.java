f9c6ed40107a1ff357873dcd1e4988cfc6e258cf
lee <spglados@gmail.com>
2024-08-29 10:40:35 +0900

    굉고기능구현중

--- src/main/java/com/example/demo/repository/AdminRepository.java
+++ src/main/java/com/example/demo/repository/AdminRepository.java
@@ -1,10 +1,33 @@
 package com.example.demo.repository;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import com.example.demo.dto.Admin;
+import com.example.demo.dto.Advertise;
+import com.example.demo.dto.User;
 
 @Mapper
 public interface AdminRepository {
 	
+	// 전체 유저수
+	public int countNumberOfUser();
+	// 전체유저 충전캐쉬
+	public int countChargeAmount();	
+	// 전체유저 사용캐쉬
+	public int countSpendAmount();
+	// 캐쉬 사용률 
+	public double countSpendAmountRate();
 	
-
+	// 광고추가 
+	public int insertAdvertise(Advertise advertise);
+	
+	// 광고 상태 변경 -- stauts
+	public int updateAdvertiseStatus();
+	
+	// 전체 광고 조회
+	public List<Advertise> selectAllAdvertise();
+	
 }
--- src/main/java/com/example/demo/service/AdminService.java
+++ src/main/java/com/example/demo/service/AdminService.java
@@ -1,8 +1,59 @@
 package com.example.demo.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.ui.Model;
+
+import com.example.demo.dto.Advertise;
+import com.example.demo.repository.AdminRepository;
 
 @Service
 public class AdminService {
+	
+	private final AdminRepository adminRepository;
+	
+	public AdminService(AdminRepository adminRepository) {
+		this.adminRepository = adminRepository;
+	}
+	
+	// 유저 수 계산
+	public int countUser() {
+		
+		return adminRepository.countNumberOfUser();
+	}
+	
+	public int countChargeCash() {
+		
+		return adminRepository.countChargeAmount();
+	}
+	
+	public double countSpendCashRate() {
+		
+		return adminRepository.countSpendAmountRate();
+	}
+	
+	// 광고 추가
+	public int insertAdvertise() {
+		
+		return adminRepository.insertAdvertise();
+	}
+	
+	// 광고 상태 변경 
+	@Transactional
+	public int updateAdvertiseStatus() {
+		
+		return adminRepository.updateAdvertiseStatus();
+	}
+	
+	// 전체 광고 조회
+	public List<Advertise> selectAllAdvertise(){
+		List<Advertise> advertiseEntity = null;
+		
+		advertiseEntity = adminRepository.selectAllAdvertise();
+		
+		return advertiseEntity;
+	}
 
 }
