d434705ca15fae26337f2e2e42c99ea92ff0f958
JunYeong Doh <secondclover714@gmail.com>
2024-08-30 18:40:21 +0900

    로그인 / 회원가입 기능 완성

--- src/main/java/com/example/demo/service/EmailSendService.java
+++ src/main/java/com/example/demo/service/EmailSendService.java
@@ -38,7 +38,36 @@ public class EmailSendService {
 
         authNumber = Integer.parseInt(randomNumber);
     }
-
+    
+    /* 아이디 찾기 메일 전송 */
+    public void idMailSend(String setFrom, String toMail, String title, String content) {
+        MimeMessage message = javaMailSender.createMimeMessage();
+        try {
+            MimeMessageHelper helper = new MimeMessageHelper(message,true,"utf-8");
+            helper.setFrom(setFrom); // service name
+            helper.setTo(toMail); // customer email
+            helper.setSubject(title); // email title
+            helper.setText(content,true); // content, html: true
+            javaMailSender.send(message);
+        } catch (MessagingException e) {
+            e.printStackTrace(); // 에러 출력
+        }
+    }
+    
+    /* 임시 비밀번호 메일 전송 */
+    public String pwMailSend(String email) {
+        makeRandomNum();
+        String customerMail = email;
+        String title = "쉘위 비밀번호 찾기 요청 메일";
+        String content =
+        		"안녕하세요! 회원님. <br>" +
+    	        		"회원님의 임시 비밀번호는 " + authNumber + "입니다. <br>" +
+    	        		"보안을 위해 로그인 후 비밀번호 변경을 추천드립니다. <br>" +
+    	        		"감사합니다.";
+        mailSend(serviceName, customerMail, title, content);
+        return Integer.toString(authNumber);
+    }
+    
     /* 이메일 전송 */
     public void mailSend(String setFrom, String toMail, String title, String content) {
         MimeMessage message = javaMailSender.createMimeMessage();
@@ -52,26 +81,27 @@ public class EmailSendService {
         } catch (MessagingException e) {
             e.printStackTrace(); // 에러 출력
         }
-        // redis에 3분 동안 이메일과 인증 코드 저장
+        // redis에 5분 동안 이메일과 인증 코드 저장
         ValueOperations<String, String> valOperations = redisConfig.redisTemplate().opsForValue();
-        valOperations.set(toMail, Integer.toString(authNumber), 180, TimeUnit.SECONDS);
+        valOperations.set(toMail, Integer.toString(authNumber), 300, TimeUnit.SECONDS);
     }
 
     /* 이메일 작성 */
     public String joinEmail(String email) {
         makeRandomNum();
         String customerMail = email;
-        String title = "회원 가입을 위한 이메일입니다!";
+        String title = "쉘위 이메일 인증번호 안내";
         String content =
-                "이메일을 인증하기 위한 절차입니다." +
-                        "<br><br>" +
-                        "인증 번호는 " + authNumber + "입니다." +
+                "안녕하세요 사용자님<br>" +
                         "<br>" +
-                        "회원 가입 폼에 해당 번호를 입력해주세요.";
+                		"요청하신 이메일 인증 번호를 안내 드립니다.<br>" +
+                		"아래 번호를 입력하여 쉘위 인증 절차를 마무리 해 주세요<br>" +
+                        "인증 번호 : " + authNumber + "<br>" +
+                        "<br>" +
+                        "본 인증 번호는 5분 간 유효합니다.";
         mailSend(serviceName, customerMail, title, content);
         return Integer.toString(authNumber);
     }
-    
     /* 인증번호 확인 */
     public Boolean checkAuthNum(String email, String authNum) {
         ValueOperations<String, String> valOperations = redisConfig.redisTemplate().opsForValue();
--- src/main/webapp/WEB-INF/view/sign/findId.jsp
+++ src/main/webapp/WEB-INF/view/sign/findId.jsp
@@ -3,10 +3,46 @@
 <!DOCTYPE html>
 <html>
 <head>
+<link rel="stylesheet" type="text/css" href="/css/login.css">
 <meta charset="UTF-8">
-<title>Insert title here</title>
+<title>아이디 찾기</title>
 </head>
 <body>
-
+	<div class="login-wrapper">
+    	<div class="upper">
+    		<div>
+        		<h2>아이디 찾기</h2>
+    	    </div>
+	        <div>
+        		<a href="main"><img class="img-concert" src="/images/logo.png"></a>
+        	</div>
+        </div>
+	
+		<form method="post" action="/user/find-id" id="login-form">
+			<h4>이름 또는 닉네임</h4>
+	    	<input type="text" name="username" placeholder="이름 또는 닉네임을 입력하세요">
+	    	<h4>이메일</h4>
+            <input class ="email-input" id="emailBody"type="text"name="emailBody" placeholder="이메일 입력">
+            @
+            <input class="email-input" type="text" name="emailDomain" id="emailDomain" placeholder="도메인 입력">
+        	<select name="domain" id="domainList">
+	          <option value="">직접 입력</option>
+	          <option value="naver.com">naver</option>
+	          <option value="hanmail.net">daum</option>
+	          <option value="gmail.com">google</option>
+       		</select>
+	    	<input type="submit" value="아이디 찾기">
+	    </form>
+	    <div class="su-btn">
+	     	<a href="find-pw">비밀번호 찾기</a> | <a href="sign-up">회원가입</a>
+	    </div>
+	</div>
 </body>
+<script type="text/javascript" src="/js/findId.js"></script>
+<script>
+    document.getElementById('domainList').addEventListener('change', function() {
+        var selectedDomain = this.value;
+        document.getElementById('emailDomain').value = selectedDomain;
+    });
+</script>
 </html>
